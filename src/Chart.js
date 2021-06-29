15618bdb232e92ba7b88eaf98bc9d1c838705222
aryamanrishabh <aryamanrishabh@gmail.com>
2021-06-29 23:46:08 +0530

    commit on master

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { CanvasJSChart } from "canvasjs-react-charts";
 
 const Chart = ({ symbol }) => {
   const [stockData, setStockData] = useState([]);
@@ -16,7 +17,7 @@ const Chart = ({ symbol }) => {
   }
 
   function formatStockData(stockData) {
-    // Convert stockData from an object to an array
+    // Converting stockData from an object to an array
     return Object.entries(stockData).map((entries) => {
       const [date, priceData] = entries;
 
@@ -33,4 +34,79 @@ const Chart = ({ symbol }) => {
   useEffect(() => {
     fetchStockData();
   }, [symbol]);
+
+  return (
+    <CanvasJSChart
+      options={{
+        axisY: {
+          // Minimum value is 10% less than the lowest price in the dataset
+          minimum: Math.min(...stockData.map((data) => data.low)) / 1.1,
+          // Minimum value is 10% more than the highest price in the dataset
+          maximum: Math.max(...stockData.map((data) => data.high)) * 1.1,
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+          },
+        },
+        axisX: {
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+          },
+          scaleBreaks: {
+            spacing: 0,
+            fillOpacity: 0,
+            lineThickness: 0,
+            customBreaks: stockData.reduce((breaks, value, index, array) => {
+              // Just return on the first iteration
+              // Since there is no previous data point
+              if (index === 0) return breaks;
+
+              // Time in UNIX for current and previous data points
+              const currentDataPointUnix = Number(new Date(value.date));
+              const previousDataPointUnix = Number(
+                new Date(array[index - 1].date)
+              );
+
+              // One day converted to milliseconds
+              const oneDayInMs = 86400000;
+
+              // Difference between the current and previous data points
+              // In milliseconds
+              const difference = previousDataPointUnix - currentDataPointUnix;
+
+              return difference === oneDayInMs
+                ? // Difference is 1 day, no scale break is needed
+                  breaks
+                : // Difference is more than 1 day, need to create
+                  // A new scale break
+                  [
+                    ...breaks,
+                    {
+                      startValue: currentDataPointUnix,
+                      endValue: previousDataPointUnix - oneDayInMs,
+                    },
+                  ];
+            }, []),
+          },
+        },
+        data: [
+          {
+            type: "candlestick",
+            dataPoints: stockData.map((stockData) => ({
+              x: new Date(stockData.date),
+              y: [
+                stockData.open,
+                stockData.high,
+                stockData.low,
+                stockData.close,
+              ],
+            })),
+          },
+        ],
+      }}
+    />
+  );
 };
+
+export default Chart;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,94 +1,66 @@
 import { useState, useEffect } from "react";
+import Chart from "./Chart";
 
 const Navbar = () => {
   const [symbol, setSymbol] = useState("");
-  const [stockData, setStockData] = useState([]);
-
-  async function fetchStockData() {
-    const res = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=AWS9D6C31M59ZV8H`
-    );
-
-    const json = await res.json();
-
-    setStockData(formatStockData(json["Time Series (Daily)"]));
-
-    //console.log(stockData);
-  }
-
-  function formatStockData(stockData) {
-    // Convert stockData from an object to an array
-    return Object.entries(stockData).map((entries) => {
-      const [date, priceData] = entries;
-
-      return {
-        date,
-        open: Number(priceData["1. open"]),
-        high: Number(priceData["2. high"]),
-        low: Number(priceData["3. low"]),
-        close: Number(priceData["4. close"]),
-      };
-    });
-  }
-
-  useEffect(() => {
-    fetchStockData();
-  }, [symbol]);
 
   return (
-    <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-      <a className="navbar-brand pl-4" href="#">
-        Navbar
-      </a>
+    <div>
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+        <a className="navbar-brand pl-4" href="#">
+          CEA
+        </a>
 
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#collapsibleNavbar"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse pr-4" id="collapsibleNavbar">
-        <ul className="navbar-nav ml-auto">
-          <li value="IBM" className="nav-item">
-            <a
-              onClick={(e) => {
-                setSymbol(e.currentTarget.innerHTML);
-              }}
-              value="IBM"
-              className="nav-link"
-              href="#"
-            >
-              IBM
-            </a>
-          </li>
-          <li value="TSLA" className="nav-item">
-            <a
-              onClick={(e) => {
-                setSymbol(e.currentTarget.innerHTML);
-              }}
-              className="nav-link"
-              href="#"
-            >
-              TSLA
-            </a>
-          </li>
-          <li value="DAX" className="nav-item">
-            <a
-              onClick={(e) => {
-                setSymbol(e.currentTarget.innerHTML);
-              }}
-              className="nav-link"
-              href="#"
-            >
-              DAX
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div className="collapse navbar-collapse pr-4" id="collapsibleNavbar">
+          <ul className="navbar-nav ml-auto">
+            <li value="IBM" className="nav-item">
+              <a
+                onClick={(e) => {
+                  setSymbol(e.currentTarget.innerHTML);
+                }}
+                value="IBM"
+                className="nav-link"
+                href="#"
+              >
+                IBM
+              </a>
+            </li>
+            <li value="TSLA" className="nav-item">
+              <a
+                onClick={(e) => {
+                  setSymbol(e.currentTarget.innerHTML);
+                }}
+                className="nav-link"
+                href="#"
+              >
+                TSLA
+              </a>
+            </li>
+            <li value="DAX" className="nav-item">
+              <a
+                onClick={(e) => {
+                  setSymbol(e.currentTarget.innerHTML);
+                }}
+                className="nav-link"
+                href="#"
+              >
+                DAX
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Chart symbol={symbol} />
+    </div>
   );
 };
 
