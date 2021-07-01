2db14b584c869e7ae7f5e255a0d38bc02ea0b948
aryamanrishabh <aryamanrishabh@gmail.com>
2021-07-01 23:25:48 +0530

    commit on master

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,62 +7,14 @@ const Navbar = () => {
 
   return (
     <div>
-      {/* <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-        <a className="navbar-brand pl-4" href="#">
-          CEA
-        </a>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse pr-4" id="collapsibleNavbar">
-          <ul className="navbar-nav ml-auto">
-            <li value="IBM" className="nav-item">
-              <a
-                onClick={(e) => {
-                  setSymbol(e.currentTarget.innerHTML);
-                }}
-                value="IBM"
-                className="nav-link"
-                href="#"
-              >
-                IBM
-              </a>
-            </li>
-            <li value="TSLA" className="nav-item">
-              <a
-                onClick={(e) => {
-                  setSymbol(e.currentTarget.innerHTML);
-                }}
-                className="nav-link"
-                href="#"
-              >
-                TSLA
-              </a>
-            </li>
-            <li value="DAX" className="nav-item">
-              <a
-                onClick={(e) => {
-                  setSymbol(e.currentTarget.innerHTML);
-                }}
-                className="nav-link"
-                href="#"
-              >
-                SHOP
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
-
       <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-        <Link className="navbar-brand pl-5" to="/">
+        <Link
+          className="navbar-brand pl-5"
+          to="/"
+          onClick={() => {
+            setSymbol("");
+          }}
+        >
           CEA
         </Link>
 
@@ -113,12 +65,36 @@ const Navbar = () => {
               >
                 TSCO
               </a>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={(e) => {
+                  setSymbol(e.currentTarget.innerHTML);
+                }}
+              >
+                RELIANCE.BSE
+              </a>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={(e) => {
+                  setSymbol(e.currentTarget.innerHTML);
+                }}
+              >
+                BABA
+              </a>
             </div>
           </li>
         </ul>
         <ul className="navbar-nav ml-auto pr-5">
           <li className="nav-item">
-            <Link className="nav-link" to="/compare">
+            <Link
+              className="nav-link"
+              to="/compare"
+              onClick={() => {
+                setSymbol("");
+              }}
+            >
               Compare
             </Link>
           </li>
