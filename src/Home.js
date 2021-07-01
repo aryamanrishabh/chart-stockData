3e00bf546a24f402f62ce36cf142cd6fb365d112
aryamanrishabh <aryamanrishabh@gmail.com>
2021-07-02 03:01:42 +0530

    final commit

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import tes from "../images/tesco.png";
 import rel from "../images/reliance.png";
 import ali from "../images/alibaba.png";
 import shop from "../images/shopify.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -11,42 +12,54 @@ const Home = () => {
       <h1 className="display-4 m-3 text-center">Popular Stocks</h1>
       <div className="row mt-5">
         <div className="col-md">
-          <div className="logo-card">
-            <img src={tsla} alt="logo" />
-            <h4 className="text-center mt-1">Tesla</h4>
-          </div>
+          <Link to="/charts/tsla" style={{ textDecoration: "none" }}>
+            <div className="logo-card">
+              <img src={tsla} alt="logo" />
+              <h4 className="text-center mt-1">Tesla</h4>
+            </div>
+          </Link>
         </div>
         <div className="col-md">
-          <div className="logo-card p-1">
-            <img src={ibm} alt="logo" />
-            <h4 className="text-center mt-1">IBM</h4>
-          </div>
+          <Link to="/charts/ibm" style={{ textDecoration: "none" }}>
+            <div className="logo-card p-1">
+              <img src={ibm} alt="logo" />
+              <h4 className="text-center mt-1">IBM</h4>
+            </div>
+          </Link>
         </div>
         <div className="col-md">
-          <div className="logo-card p-2">
-            <img src={shop} alt="logo" />
-            <h4 className="text-center mt-1">Shopify</h4>
-          </div>
+          <Link to="/charts/shop" style={{ textDecoration: "none" }}>
+            <div className="logo-card p-2">
+              <img src={shop} alt="logo" />
+              <h4 className="text-center mt-1">Shopify</h4>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="row mt-5">
         <div className="col-md">
-          <div className="logo-card p-2">
-            <img src={tes} alt="logo" />
-            <h4 className="text-center mt-1">TESCO</h4>
-          </div>
+          <Link to="/charts/tsco" style={{ textDecoration: "none" }}>
+            <div className="logo-card p-2">
+              <img src={tes} alt="logo" />
+              <h4 className="text-center mt-1">TESCO</h4>
+            </div>
+          </Link>
         </div>
         <div className="col-md">
-          <div className="logo-card p-2">
-            <img src={rel} alt="logo" />
-            <h4 className="text-center mt-1">Reliance</h4>
-          </div>
+          <Link to="/charts/reliance.bse" style={{ textDecoration: "none" }}>
+            <div className="logo-card p-2">
+              <img src={rel} alt="logo" />
+              <h4 className="text-center mt-1">Reliance</h4>
+            </div>
+          </Link>
         </div>
         <div className="col-md">
-          <div className="logo-card p-2">
-            <img src={ali} alt="logo" />
-            <h4 className="text-center mt-1">Alibaba</h4>
-          </div>
+          <Link to="/charts/baba" style={{ textDecoration: "none" }}>
+            <div className="logo-card p-2">
+              <img src={ali} alt="logo" />
+              <h4 className="text-center mt-1">Alibaba</h4>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
